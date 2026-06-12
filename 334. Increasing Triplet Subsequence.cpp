aa219e369d6cb819d63c9caeaad151c498677106
Mustufa_MNNIT <mustufaca060@gmail.com>
2026-06-12 21:46:03 +0530

    Create 334. Increasing Triplet Subsequence.cpp

---
+++ 334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,24 @@
+//this code solve by greedy aproch O(n) learn again//
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for (int x : nums) {
+
+            if (x <= first) {
+                first = x;
+            }
+            else if (x <= second) {
+                second = x;
+            }
+            else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
